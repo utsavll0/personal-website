0256a71ff6cd222d6dd64819acc70ac8b6fbddff
Utsav Lal <ualal@ncsu.edu>
2024-05-01 02:17:18 -0400

    update meta data

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,7 +2,7 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://sveltekit-static-blog-template.vercel.app/';
+export const siteBaseUrl = 'https://utsavkumar-lal.vercel.app/';
 
 export const keywords = ['Svelte', 'SvelteKit', 'Template', 'Blog', 'Starter', 'Static Site'];
 
@@ -10,4 +10,4 @@ export const description = 'Hello, welcome to my personal website';
 
 export const title = 'Utsav Lal';
 
-export const image = `${siteBaseUrl}/images/site-preview.png`;
+export const image = `${siteBaseUrl}/images/utsav_at_beach.jpg`;
